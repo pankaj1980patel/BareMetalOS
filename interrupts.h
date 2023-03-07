4686b296c1830c71a9f91f2774ed9968b7e120b0
pankaj1980patel <pantherpatel1980@gamil.com>
2023-03-07 13:31:33 +0530

    interrupts.h

---
+++ interrupts.h
@@ -0,0 +1,38 @@
+#ifndef __INTERRUPTS_H
+#define __INTERRUPTS_H
+
+#include "types.h"
+#include "port.h"
+class InterruptManager{
+    protected:
+        struct GateDescriptor
+        {
+            uint16_t handlerAddressLowBits;
+            uint16_t gdt_codeSegmentSelector;
+            uint8_t reserved;
+            uint8_t access;
+            uint16_t handlerAddressHighBits;
+        } __attribute__((packed));
+
+        static GateDescriptor interruptDescriptorTable[256];
+
+        static void SetInterruptDescriptorTableEntry(
+            uint8_t interruptNumber,
+            uint16_t codeSegmentSelectorOffset,
+            void (*handle)(),
+            uint8_t DescriptorPrivateOffset,
+            uint8_t DescriptorType
+        );
+    public:
+        InterruptManager(GlobalDescriptorTable *gdt);
+        ~InterruptManager();
+
+        static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
+
+        static void IgnoreInterruptRequest();
+        static void HandleInterruptRequest0x00();
+        static void HandleInterruptRequest0x01();
+};
+
+
+#endif
